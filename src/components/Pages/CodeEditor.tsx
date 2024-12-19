b26275f546992d23a5bf0d10f4ce10ec246fbb5e
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2024-12-19 14:09:32 +0530

    Code Editor Completed.

--- src/components/Pages/CodeEditor.tsx
+++ src/components/Pages/CodeEditor.tsx
@@ -1,13 +1,51 @@
 import { Editor } from "@monaco-editor/react";
+import { styled } from "styled-components";
+
+const getCode = (value: any) => {
+  code = value;
+}
+
+let code = ""
+
+const BtnStyle = styled.button`
+  background: linear-gradient(
+    90deg,
+    rgba(253, 29, 29, 1) 0%,
+    rgba(252, 176, 69, 1) 100%
+  );
+  color: black;
+  font-size: 15px;
+  font-weight: bold;
+`
 
 const CodeEditor = () => {
+
   return (
-    <Editor
-      height="87vh"
-      defaultLanguage="python"
-      defaultValue="#code here"
-      theme="vs-dark"
-    />
+    <div className="d-flex flex-column justify-content-between">
+
+      <div style={{flex: 1}} className="border p-3">
+        <Editor
+          height="85vh"
+          defaultLanguage="python"
+          defaultValue="#code here"
+          theme="vs"
+          onChange={getCode}
+          options={{
+            fontSize: 20,
+            scrollbar: {
+              vertical: "hidden", 
+              horizontal: "visible", 
+            }
+          }}
+        />
+      </div>
+      
+      <div style={{flex: 1}} className="d-flex border justify-content-end">
+        <button className="btn btn-primary m-2 pt-2 pb-2 pe-4  ps-4 border" onClick={() => console.log(code)}>Run</button>
+        <BtnStyle className="btn m-2 pt-2 pb-2 pe-4  ps-4 border" onClick={() => console.log(code)}>Time</BtnStyle>
+      </div>
+      
+    </div>
   );
 };
 
--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -5,14 +5,12 @@ const FlexContanier = styled.div`
   width: 92%;
   height: 95vh;
 
-  background-color: aqua;
   display: flex;
   justify-content: space-around;
 `;
 
 const EditorContainer = styled.div`
   flex: 2;
-  background-color: green;
 `;
 
 const Result = styled.div`
@@ -20,16 +18,20 @@ const Result = styled.div`
   background-color: blue;
 `;
 
+// maintaining the code
 const Home = () => {
+  
   return (
     <FlexContanier>
+
       <EditorContainer>
         <CodeEditor />
-        <button className="btn btn-primary m-2 pt-2 pb-2 pe-4  ps-4">
-          Run
-        </button>
       </EditorContainer>
-      <Result></Result>
+
+
+      <Result>
+        
+      </Result>
     </FlexContanier>
   );
 };
